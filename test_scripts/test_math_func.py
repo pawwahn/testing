9690e7a895aa2e3cd8d51d5a096cb0c647fe39af
pawwahn <pavan.skt@gmail.com>
2019-08-27 18:43:14 +0530

    math func

--- test_scripts/test_math_func.py
+++ test_scripts/test_math_func.py
@@ -1,4 +1,5 @@
 #from unit_testing.pytest_folder import math_func
+#!/usr/bin/python3
 from files import math_func
 
 
@@ -10,7 +11,7 @@ def test_add():
 
 
 def test_prod():
-    assert math_func.prod(2, 4) == 81
+    assert math_func.prod(2, 4) != 81
     assert math_func.prod(4) == 8
     assert math_func.prod(6, 4) == 24
 
